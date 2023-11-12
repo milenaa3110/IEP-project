cbf9db759d1e8a1b0a7377f6e7f75edad5354d50
Milenajov <jmilena3110@gmail.com>
2023-11-12 14:37:59 +0100

    change name

--- projekat/configuration.py
+++
@@ -1,8 +0,0 @@
-from datetime import timedelta
-import os
-
-
-dbUrl = os.environ["DATABASE_URL"]
-
-class Configuration():
-    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://root:root@{dbUrl}/shopdb"
